9998e1f661fd64b900615f7563ce911f7e7334f7
Carly Raphan <craphan@highpoint.edu>
2019-05-03 06:04:35 -0400

    finally got .xcdatamodel file to work

---
+++ Photo+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Photo+CoreDataClass.swift
+//  ImageMatch
+//
+//  Created by Carly Raphan on 5/3/19.
+//  Copyright © 2019 highpoint.edu. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Photo)
+public class Photo: NSManagedObject {
+
+}
---
+++ Photo+CoreDataProperties.swift
@@ -0,0 +1,25 @@
+//
+//  Photo+CoreDataProperties.swift
+//  ImageMatch
+//
+//  Created by Carly Raphan on 5/3/19.
+//  Copyright © 2019 highpoint.edu. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension Photo {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Photo> {
+        return NSFetchRequest<Photo>(entityName: "Photo")
+    }
+
+    @NSManaged public var photoID: String?
+    @NSManaged public var title: String?
+    @NSManaged public var dateTaken: NSDate?
+    @NSManaged public var remoteURL: NSURL?
+
+}
